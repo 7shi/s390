8297c6880214f0ba1f33203ad16757a5fdc21974
7shi <7shi@live.jp>
2015-09-25 23:38:49 +0900

    まとめて書き込むことで高速化

--- long.sh
+++ long.sh
@@ -5,7 +5,6 @@ as=$arch-as
 dis="$arch-objdump -d"
 
 t=long
-rm -f $t.s
 
 is=`grep long allop.d | cut -d' ' -f1`
 js=`for i in {0..255}; do printf "%02x " $i; done`
@@ -14,9 +13,9 @@ for i in $is
 do
     for j in $js
     do
-        echo ".byte 0x$i, 0x$j, 7, 7, 7, 7" >> $t.s
+        echo ".byte 0x$i, 0x$j, 7, 7, 7, 7"
     done
-done
+done > $t.s
 
 $as -o $t.o $t.s
 $dis $t.o > $t.d
